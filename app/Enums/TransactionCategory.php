806566ac08e310815ac08930bb55bb2f630eec68
irvan <gtrskyline2006@gmail.com>
2026-07-14 18:40:17 +0700

    add many category

--- app/Enums/TransactionCategory.php
+++ app/Enums/TransactionCategory.php
@@ -4,12 +4,37 @@ namespace App\Enums;
 
 enum TransactionCategory: string
 {
+    case SALARY = 'salary';
+    case BONUS = 'bonus';
+    case FREELANCE = 'freelance';
+    case BUSINESS = 'business';
+    case INVESTMENT = 'investment';
+    case GIFT = 'gift';
+    case REFUND = 'refund';
+    case OTHER_INCOME = 'other_income';
+
     case FOOD = 'food';
     case TRANSPORTATION = 'transportation';
+    case SHOPPING = 'shopping';
+    case ENTERTAINMENT = 'entertainment';
+    case BILLS = 'bills';
+    case RENT = 'rent';
+    case HEALTH = 'health';
+    case EDUCATION = 'education';
+    case TRAVEL = 'travel';
+    case SUBSCRIPTION = 'subscription';
+    case INSURANCE = 'insurance';
+    case TAX = 'tax';
+    case CHARITY = 'charity';
+    case PERSONAL_CARE = 'personal_care';
+    case OTHER_EXPENSE = 'other_expense';
 
-    public function label()
+    public function label(): string
     {
-        return str($this->value)->replace('_', ' ')->title();
+        return str($this->value)
+            ->replace('_', ' ')
+            ->title()
+            ->toString();
     }
 
     public static function values(): array
